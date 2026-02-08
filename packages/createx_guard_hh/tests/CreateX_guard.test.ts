4b31baa1eafaa81b944f7b58895934c4d485698e
lime <liumingxun@yeah.net>
2026-02-08 17:42:28 +0800

    feat: append eip55 address compute guard salt test

--- packages/createx_guard_hh/tests/CreateX_guard.test.ts
+++ packages/createx_guard_hh/tests/CreateX_guard.test.ts
@@ -1,11 +1,12 @@
-import type { Address, Hex } from 'viem'
+import type { Hex } from 'viem'
 import { computeGuardedSalt as _guard } from 'createx_guard'
 import { network } from 'hardhat'
-import { ethAddress as ANY_ADDRESS, zeroAddress as ZERO_ADDRESS } from 'viem'
+import { isAddress, ethAddress as SELF_ADDRESS, zeroAddress as ZERO_ADDRESS } from 'viem'
 import { describe, expect, it } from 'vitest'
 import GuardModule from '../ignition/modules/CreateX_guard'
 
-const SELF_ADDRESS: Address = '0x1111111111111111111111111111111111111111' as const
+const ANY_ADDRESS = '0x1111111111111111111111111111111111111111' as const
+const EIP55_SELF_ADDRESS = '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE' as const
 
 const SELF_XC_OFF_SALT: Hex = `${SELF_ADDRESS}00${'aa'.repeat(11)}`
 const SELF_XC_ON_SALT: Hex = `${SELF_ADDRESS}01${'aa'.repeat(11)}`
@@ -37,7 +38,7 @@ const passed_salt_cases = [
   { salt: ANY_XC_NA_SALT, msgSender: SELF_ADDRESS },
 ] as const
 
-const reverted_salt_cases = [
+const invalid_salt_cases = [
   // row 1: Self, NA
   { salt: SELF_XC_NA_SALT, msgSender: SELF_ADDRESS, chainId: 31337 },
   { salt: SELF_XC_NA_SALT, msgSender: SELF_ADDRESS },
@@ -46,33 +47,50 @@ const reverted_salt_cases = [
   { salt: ZERO_XC_NA_SALT, msgSender: SELF_ADDRESS },
 ] as const
 
-describe('guard module', async () => {
-  const { viem: hviem, ignition } = await network.connect()
-  const caller = await hviem.getTestClient({
-    account: SELF_ADDRESS,
-  })
-  const { guard: createx_guard } = await ignition.deploy(GuardModule)
+const { viem: hviem, ignition } = await network.connect()
+const caller = await hviem.getTestClient({
+  account: SELF_ADDRESS,
+})
+const eip55_caller = await hviem.getTestClient({
+  account: EIP55_SELF_ADDRESS,
+})
+const { guard: createx_guard } = await ignition.deploy(GuardModule)
 
-  it.skipIf(createx_guard.address === '0x5FbDB2315678afecb367f032d93F642f64180aa3')('basic', async () => {
+describe('guard module', async () => {
+  it('eip55 is valid address', async () => {
+    expect(isAddress(EIP55_SELF_ADDRESS)).toBe(true)
+  })
+
+  it.skipIf(createx_guard.address === '0x5FbDB2315678afecb367f032d93F642f64180aa3')('sanity check: test environment is set up correctly', async () => {
     expect(createx_guard).toBeDefined()
     expect(createx_guard.read._guard).toBeDefined()
     expect(createx_guard.address).toMatchInlineSnapshot(`"0x5FbDB2315678afecb367f032d93F642f64180aa3"`)
-    expect(caller.account?.address).toMatchInlineSnapshot(`"0x1111111111111111111111111111111111111111"`)
+    expect(eip55_caller.account?.address).toMatchInlineSnapshot(`"0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE"`)
+    expect(caller.account?.address).toMatchInlineSnapshot(`"0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee"`)
     expect(caller.chain.id).toMatchInlineSnapshot(`31337`)
   })
 
   it.for(passed_salt_cases)('%$: matches _guard output for valid salt combinations (Typescript vs contract)', async (input) => {
     if (!caller.account)
       throw new Error('No account found for caller')
+    if (!eip55_caller.account)
+      throw new Error('No account found for eip55_caller')
 
     const contractResult = await createx_guard.read._guard([input.salt], {
       account: caller.account,
     })
+    const eip55_contractResult = await createx_guard.read._guard([input.salt], {
+      account: eip55_caller.account,
+    })
+    const typescriptResult = _guard(input)
+    const eip55_typescriptResult = _guard({ ...input, msgSender: EIP55_SELF_ADDRESS })
 
-    expect(_guard(input)).toEqual(contractResult)
+    expect(eip55_contractResult).toEqual(contractResult)
+    expect(typescriptResult).toEqual(contractResult)
+    expect(eip55_typescriptResult).toEqual(contractResult)
   })
 
-  it.for(reverted_salt_cases.filter(v => v.salt.startsWith(SELF_ADDRESS)))('%$: (Typescript) rejects invalid salt', async (input) => {
+  it.for(invalid_salt_cases.filter(v => v.salt.startsWith(ANY_ADDRESS)))('%$: (Typescript) rejects invalid salt', async (input) => {
     if (!caller.account)
       throw new Error('No account found for caller')
 
@@ -92,11 +110,11 @@ describe('guard module', async () => {
     `, 'When the first 20 bytes matches the caller address or is zero address, the cross-chain redeploy protection flag must be explicitly specified as either 0 or 1 at the 21st byte.')
   })
 
-  it.for(reverted_salt_cases.filter(v => v.salt.startsWith(SELF_ADDRESS)))('%$: (contract) reverts invalid salt', async (input) => {
+  it.for(invalid_salt_cases.filter(v => v.salt.startsWith(ANY_ADDRESS)))('%$: (contract) reverts invalid salt', async (input) => {
     if (!caller.account)
       throw new Error('No account found for caller')
 
-    expect(() =>
+    await expect(() =>
       createx_guard.read._guard([input.salt], {
         account: caller.account,
       }),
@@ -119,7 +137,7 @@ describe('guard module', async () => {
     `)
   })
 
-  it.for(reverted_salt_cases.filter(v => v.salt.startsWith(ZERO_ADDRESS)))('%$: (Typescript) rejects invalid salt', async (input) => {
+  it.for(invalid_salt_cases.filter(v => v.salt.startsWith(ZERO_ADDRESS)))('%$: (Typescript) rejects invalid salt', async (input) => {
     if (!caller.account)
       throw new Error('No account found for caller')
 
@@ -139,11 +157,11 @@ describe('guard module', async () => {
     `, 'When the first 20 bytes matches the caller address or is zero address, the cross-chain redeploy protection flag must be explicitly specified as either 0 or 1 at the 21st byte.')
   })
 
-  it.for(reverted_salt_cases.filter(v => v.salt.startsWith(ZERO_ADDRESS)))('%$: (contract) reverts invalid salt', async (input) => {
+  it.for(invalid_salt_cases.filter(v => v.salt.startsWith(ZERO_ADDRESS)))('%$: (contract) reverts invalid salt', async (input) => {
     if (!caller.account)
       throw new Error('No account found for caller')
 
-    expect(() =>
+    await expect(() =>
       createx_guard.read._guard([input.salt], {
         account: caller.account,
       }),
@@ -151,7 +169,7 @@ describe('guard module', async () => {
       [ContractFunctionExecutionError: An unknown RPC error occurred.
 
       Raw Call Arguments:
-        from:  0x1111111111111111111111111111111111111111
+        from:  0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
         to:    0x5FbDB2315678afecb367f032d93F642f64180aa3
         data:  0xbdfd4b140000000000000000000000000000000000000000ffbbbbbbbbbbbbbbbbbbbbbb
        
